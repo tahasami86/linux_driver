92c7eee99b83ea4b78e5ac381823c189c866fab9
tahasami <tahasami8@gmail.com>
2025-04-21 08:01:10 +0000

    Added Config read/write capabilities in the driver
    Signed-off-by: tahasami <tahasami8@gmail.com>

--- main.c
+++ main.c
@@ -6,8 +6,8 @@ MODULE_LICENSE("GPL");
 MODULE_AUTHOR("Taha Sami <tahasami8@gmail.com>");
 MODULE_DESCRIPTION("Hello, world! driver for Testing");
 
-#define VENDOR_ID 0x8086
-#define DEVICE_ID 0x29c0
+#define VENDOR_ID 0x1234
+#define DEVICE_ID 0x1111
 
 static const struct pci_device_id my_pci_tbl[] = {
     { PCI_DEVICE(VENDOR_ID, DEVICE_ID) },
@@ -17,11 +17,93 @@ static const struct pci_device_id my_pci_tbl[] = {
 MODULE_DEVICE_TABLE(pci,my_pci_tbl);
 
 
+
+static int config_space_read(struct pci_dev *pdev,const struct pci_device_id *ent){
+    
+    u16 vid, did;
+    u8 capability_ptr;
+    u32 bar0, saved_bar0;
+
+    pr_info("%s %d Entering config space read \n", __func__, __LINE__);
+
+    /*Reading VENDOR_ID and DEVICE_ID*/
+    if(0 != pci_read_config_word(pdev, 0x0, &vid)){
+        pr_err("%s %d Failed to read VENDOR_ID\n", __func__, __LINE__);
+        return -1;
+    }
+
+    pr_info("%s %d VENDOR_ID: %04x\n", __func__, __LINE__,vid);
+
+    if(0 != pci_read_config_word(pdev,0x2, &did)){
+        pr_err("%s %d Failed to read DEVICE_ID\n", __func__, __LINE__);
+        return -1;
+    }
+
+    pr_info("%s %d DEVICE_ID: %04x\n", __func__, __LINE__,did);
+    
+    /*Reading capability_ptr*/
+
+    if(0 != pci_read_config_byte(pdev,0x34, &capability_ptr)){
+        pr_err("%s %d Failed to read capability_ptr\n", __func__, __LINE__);
+        return -1;
+    }
+    if(capability_ptr)
+        pr_info("%s %d capability_ptr: %08x The PCI has capabilities\n", __func__, __LINE__,capability_ptr);
+    else
+        pr_info("%s %d capability_ptr: %08x The PCI has no capabilities\n", __func__, __LINE__,capability_ptr);
+
+    /*Reading BAR0*/
+    if(0 != pci_read_config_dword(pdev, 0x10, &bar0)){
+        pr_err("%s %d Failed to read BAR0\n", __func__, __LINE__);
+        return -1;
+    }
+
+    saved_bar0 = bar0;
+    pr_info("%s %d BAR0 After Reading is: %04X \n", __func__, __LINE__,bar0);
+
+    if(0 != pci_write_config_dword(pdev, 0x10, 0xffffffff)){
+        pr_err("%s %d Failed to write BAR0\n", __func__, __LINE__);
+        return -1;
+    }
+    /*Reading BAR0*/
+    if(0 != pci_read_config_dword(pdev, 0x10, &bar0)){
+    pr_err("%s %d Failed to read BAR0\n", __func__, __LINE__);
+    return -1;
+    }
+
+    pr_info("%s %d After writing to BAR0: %04X\n", __func__, __LINE__,bar0);
+
+    if(bar0 & 0x3 == 1)
+        pr_info(" BAR0 is IO space\n");
+    else
+        pr_info("BAR0 is memory space\n");
+
+    bar0 &= 0xfffffffD;
+    bar0 = ~bar0;
+    bar0++;
+
+    pr_info("%s %d  BAR0 size is %u bytes:\n", __func__, __LINE__,bar0);
+
+    if(0 != pci_write_config_dword(pdev, 0x10, saved_bar0)){
+        pr_err("%s %d Failed to write BAR0\n", __func__, __LINE__);
+        return -1;
+    }   
+
+    return 0;
+    
+}
+
 static int my_pci_probe(struct pci_dev *pdev, const struct pci_device_id *ent){
 
-    printk("%s %d probing device %04x:%04x\n", __func__, __LINE__,pdev->vendor, pdev->device);
+    int err=0;
+    printk("%s %d probing device \n", __func__, __LINE__);
+
+    if(err != config_space_read(pdev,ent)){
+        return -1;
+    }
     return 0;
 }
+
 
 static void my_pci_remove(struct pci_dev *pdev){
 
